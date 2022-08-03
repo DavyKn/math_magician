3c74b973238f0240ba6bd3f7197408fd5ddd4542
DavyKK <davykk04@gmail.com>
2022-08-03 13:41:36 +0300

    calculator UI created

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import { Component } from 'react';
+import Calculator from './components/Calculator';
 
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Calculator name="Davis" />
+    );
+  }
+}
+
+export default App;
