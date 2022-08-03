bf46be2bee1f96fc48be0937f5a2d47bfa2e0cac
DavyKK <davykk04@gmail.com>
2022-08-03 14:23:10 +0300

    linter error fixed, UI rendered

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Component } from 'react';
+import React from 'react';
+
 import Calculator from './components/Calculator';
 
-
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
@@ -10,7 +10,7 @@ class App extends Component {
 
   render() {
     return (
-      <Calculator name="Davis" />
+      <Calculator />
     );
   }
 }
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,39 +1,35 @@
+/* eslint-disable react/prefer-stateless-function , no-useless-constructor */
+
 import React from 'react';
 
-
 class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
     return (
       <div className="calc_body">
-      <div className="output"><h2>0</h2></div>
-      <div className="button">
-      <button type='button' className="clear-all">AC</button>
-      <button type='button'>+/-</button>
-      <button type='button'>%</button>
-      <button type='button' className="far-right">/</button>
-      <button type='button'>7</button>
-      <button type='button'>8</button>
-      <button type='button'>9</button>
-      <button type='button' className="far-right">x</button>
-      <button type='button'>4</button>
-      <button type='button'>5</button>
-      <button type='button'>6</button>
-      <button type='button' className="far-right">-</button>
-      <button type='button'>1</button>
-      <button type='button'>2</button>
-      <button type='button'>3</button>
-      <button  type='button' className="far-right">+</button>
-      <button type='button' className="half">0</button>
-      <button type='button'>.</button>
-      <button type='button' className="far-right">=</button>
+        <div className="output"><h2>0</h2></div>
+        <div className="button">
+          <button type="button" className="clear-all">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button" className="far-right">/</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button" className="far-right">x</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button" className="far-right">-</button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button" className="far-right">+</button>
+          <button type="button" className="half">0</button>
+          <button type="button">.</button>
+          <button type="button" className="far-right">=</button>
+        </div>
       </div>
-    </div>
-   
+
     );
   }
 }
