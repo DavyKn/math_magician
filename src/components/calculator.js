09b697236c9872ef0cba714a4b05859c392a276b
DavyKK <davykk04@gmail.com>
2022-08-11 02:15:22 +0300

    final webite setup using react router

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -13,28 +13,33 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calc_body">
-      <div className="output"><h2>{state.next || state.total || 0}</h2></div>
-      <div className="button">
-        <button name="AC" type="button" className="clear-all" onClick={handleClick}>AC</button>
-        <button name="+/-" type="button" onClick={handleClick}>+/-</button>
-        <button name="%" type="button" onClick={handleClick}>%</button>
-        <button name="÷" type="button" className="far-right" onClick={handleClick}>÷</button>
-        <button name="7" type="button" onClick={handleClick}>7</button>
-        <button name="8" type="button" onClick={handleClick}>8</button>
-        <button name="9" type="button" onClick={handleClick}>9</button>
-        <button name="x" type="button" className="far-right" onClick={handleClick}>x</button>
-        <button name="4" type="button" onClick={handleClick}>4</button>
-        <button name="5" type="button" onClick={handleClick}>5</button>
-        <button name="6" type="button" onClick={handleClick}>6</button>
-        <button name="-" type="button" className="far-right" onClick={handleClick}>-</button>
-        <button name="1" type="button" onClick={handleClick}>1</button>
-        <button name="2" type="button" onClick={handleClick}>2</button>
-        <button name="3" type="button" onClick={handleClick}>3</button>
-        <button name="+" type="button" className="far-right" onClick={handleClick}>+</button>
-        <button name="0" type="button" className="half" onClick={handleClick}>0</button>
-        <button name="." type="button" onClick={handleClick}>.</button>
-        <button name="=" type="button" className="far-right" onClick={handleClick}>=</button>
+    <div className="calc_container">
+      <div className="calc_intro">
+        <h2>Lets do some Math</h2>
+      </div>
+      <div className="calc_body">
+        <div className="output"><h2>{state.next || state.total || 0}</h2></div>
+        <div className="button">
+          <button name="AC" type="button" className="clear-all" onClick={handleClick}>AC</button>
+          <button name="+/-" type="button" onClick={handleClick}>+/-</button>
+          <button name="%" type="button" onClick={handleClick}>%</button>
+          <button name="÷" type="button" className="far-right" onClick={handleClick}>÷</button>
+          <button name="7" type="button" onClick={handleClick}>7</button>
+          <button name="8" type="button" onClick={handleClick}>8</button>
+          <button name="9" type="button" onClick={handleClick}>9</button>
+          <button name="x" type="button" className="far-right" onClick={handleClick}>x</button>
+          <button name="4" type="button" onClick={handleClick}>4</button>
+          <button name="5" type="button" onClick={handleClick}>5</button>
+          <button name="6" type="button" onClick={handleClick}>6</button>
+          <button name="-" type="button" className="far-right" onClick={handleClick}>-</button>
+          <button name="1" type="button" onClick={handleClick}>1</button>
+          <button name="2" type="button" onClick={handleClick}>2</button>
+          <button name="3" type="button" onClick={handleClick}>3</button>
+          <button name="+" type="button" className="far-right" onClick={handleClick}>+</button>
+          <button name="0" type="button" className="half" onClick={handleClick}>0</button>
+          <button name="." type="button" onClick={handleClick}>.</button>
+          <button name="=" type="button" className="far-right" onClick={handleClick}>=</button>
+        </div>
       </div>
     </div>
   );
